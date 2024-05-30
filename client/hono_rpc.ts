3155d0edd24ff741c10329f35ece9f8cbbb2e577
Artyom Silantiev <artyom.silantiev@gmail.com>
2024-05-30 18:12:28 +0300

    add example upload

--- client/hono_rpc.ts
+++ client/hono_rpc.ts
@@ -1,27 +1,45 @@
 import type { HonoAppType } from '!src/app_main';
 import { hc } from 'hono/client';
+import * as fse from 'fs-extra';
 
 const client = hc<HonoAppType>('http://localhost:3000/');
 
 async function callHello() {
   const res = await client.example.hello.$get();
-  const url = client.example.hello.$url();
-  console.log(url);
+
   if (res.ok) {
     const data = await res.json();
-    console.log('res data', data);
+    console.log('callHello res data', data);
   }
 }
 
 async function callPing() {
   const res = await client.example.ping.$get();
-  const url = client.example.hello.$url();
-  console.log(url);
+
   if (res.ok) {
     const data = await res.json();
-    console.log('res data', data);
+    console.log('callPing res data', data);
   }
 }
 
-callHello();
-callPing();
+async function callUpload() {
+  const fileData = await fse.readFile(__filename);
+  const blob = new Blob([fileData]);
+  const file = new File([blob], 'hono_rpc.ts') as string | File;
+
+  const res = await client.example.upload.$post({
+    form: {
+      data: Date.now().toString(),
+      file,
+    },
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    console.log('callUpload res data', data);
+  }
+}
+
+await callHello();
+await callPing();
+await callUpload();
--- src/app_main/packs/example/example.pack.ts
+++ src/app_main/packs/example/example.pack.ts
@@ -1,4 +1,9 @@
 import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { Readable } from 'stream';
 
 const router = new Hono()
   .get(
@@ -17,7 +22,29 @@ const router = new Hono()
   )
   .get('/ping', (c) => {
     return c.json('pong');
-  });
+  })
+  .post(
+    '/upload',
+    zValidator(
+      'form',
+      z.object({
+        data: z.string(),
+        file: z.custom<File>((val) => val instanceof File),
+      }),
+    ),
+    async (c) => {
+      const body = await c.req.parseBody();
+      const file = body['file'] as File;
+      const webRs = file.stream();
+      const rs = Readable.fromWeb(webRs);
+      const ws = createWriteStream(join(process.cwd(), 'work', file.name));
+      rs.pipe(ws);
+
+      return c.json({
+        message: 'file loaded',
+      });
+    },
+  );
 
 export const ExamplePack = {
   honoRouter: router,
